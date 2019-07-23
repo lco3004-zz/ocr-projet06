d09252f5c054bc95d87c57153513b42061a887b9
Laurent Cordier <laurent.cordier3004@gmail.com>
2019-07-23 07:48:57 +0200

    simplification des repertoires properties utilisation des annotation JEE6 pour sevlet (web.xml mindique servlet mapping hs ? raison inconnue)

---
+++ webapp/src/main/java/fr/ocr/prj06/servlets/welcomeServlet.java
@@ -0,0 +1,43 @@
+package fr.ocr.prj06.servlets;
+
+import fr.ocr.prj06.msghtml.MessageDeBase;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static fr.ocr.prj06.msghtml.MessageDeBase.*;
+
+//@WebServlet(name = "homeServlet", urlPatterns = { "/home" })
+
+@WebServlet(name = "welcomeServlet", urlPatterns = { "/home" ,"/welcome"})
+public class welcomeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            response.setContentType(MessageDeBase.CONTENT_TYPE.getValeur());
+            out.print(HTML_DEBUT.getValeur());
+            out.print("<h3> OnlineStore , votre site multemedia en ligne </h3>");
+            out.print("Bonjour : "+request.getSession().getAttribute(CODESESSION_LOGIN.getValeur()) +BR.getValeur());
+            out.print(" <a href=\"ajouter-travail-form.html\"> Ajouter un article au catalogue</a>");
+            out.print(BR.getValeur());
+            out.print("<a href=\"catalogue\">Voir le Catalogue</a>");
+            out.print(BR.getValeur());
+            out.print("<a  href=\"login.html\">se connecter</a>");
+            out.print(BR.getValeur());
+            out.print("<a href=\"logout\">se déconnecter</a>");
+            out.print(HTML_FIN.getValeur());
+            out.flush();
+        }
+
+
+    }
+}
+
--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,7 +1,11 @@
 <html>
 <body>
-Hop
+<h2>
+    Projet 06
+</h2>
+<br>
 <a href="/home"> Home </a>
+<br>
 <a href="about.jsp"> about</a>
 </body>
 </html>
