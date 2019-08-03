bb5a190ffa48e7459b21122df80351b92cb3e360
Laurent Cordier <laurent.cordier3004@gmail.com>
2019-08-03 19:44:29 +0200

    corrections chemins <entities>.xml dans hibernate.cfg.xml

--- consumer/src/main/java/fr/ocr/prj06/dao/implementation/hibernate/AppHibernate.java
+++ consumer/src/main/java/fr/ocr/prj06/dao/implementation/hibernate/AppHibernate.java
@@ -1,17 +1,19 @@
 package fr.ocr.prj06.dao.implementation.hibernate;
 
+import fr.ocr.prj06.dao.implementation.hibernate.stubs.dbLongueurEntity;
 import fr.ocr.prj06.dao.implementation.hibernate.stubs.dbUtilisateurEntity;
 import fr.ocr.prj06.logs.LogsProjet;
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.Level;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 import static fr.ocr.prj06.dao.implementation.hibernate.tools.HibernateUtil.getSession;
-import static fr.ocr.prj06.messages.Messages.InfosMessages.LANCEMENT_APPLICATION;
 import static fr.ocr.prj06.logs.LogsProjet.geLogsInstance;
+import static fr.ocr.prj06.messages.Messages.InfosMessages.LANCEMENT_APPLICATION;
 
 public class AppHibernate {
 
@@ -26,10 +28,15 @@ public class AppHibernate {
                 try {
                     session.beginTransaction();
                     dbUtilisateurEntity x =null;
+                    dbLongueurEntity y = null;
                     x  = session.get(dbUtilisateurEntity.class,1);
 
                     System.out.println("-> email :" +x.getEmail()+" nom :"+x.getNom());
                     x.setNom(x.getNom()+" moi !!");
+                    List<dbLongueurEntity> result = session.createQuery( "from dbLongueurEntity " ).list();
+                    for ( dbLongueurEntity longueurEntity : (List<dbLongueurEntity>) result ) {
+                        System.out.println( "dbLongueurEntity (" + longueurEntity.getCotation() + ") : " + longueurEntity.getNom() );
+                    }
                     session.getTransaction().commit();
 
                 }catch (HibernateException ex1) {
