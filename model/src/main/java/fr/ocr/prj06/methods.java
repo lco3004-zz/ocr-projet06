b1b890f927414b79e44a9de3c85df79ec0a3edde
Laurent Cordier <laurent.cordier3004@gmail.com>
2019-08-06 16:56:13 +0200

    ajout module model

---
+++ model/src/main/java/fr/ocr/prj06/methods.java
@@ -0,0 +1,10 @@
+package fr.ocr.prj06;
+
+/**
+ * Hello world!
+ */
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ model/src/test/java/fr/ocr/prj06/methodsTest.java
@@ -0,0 +1,19 @@
+package fr.ocr.prj06;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple methods.
+ */
+public class methodsTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
