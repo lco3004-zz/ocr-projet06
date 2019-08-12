51db5346d1886230cdc2d1322e866115ea4cc10c
Laurent Cordier <laurent.cordier3004@gmail.com>
2019-08-12 17:38:48 +0200

    Ajout Logs du Projet dans UserMgMt (et Business/App qui est provisoire)

--- business/src/main/java/fr/ocr/prj06/business/App.java
+++ business/src/main/java/fr/ocr/prj06/business/App.java
@@ -1,12 +1,17 @@
 package fr.ocr.prj06.business;
 
 import fr.ocr.prj06.business.businessCtrl.UserMgmt;
+import fr.ocr.prj06.utility.logs.LogsProjet;
+import org.apache.logging.log4j.Level;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws Exception {
+        LogsProjet.geLogsInstance(App.class).maTrace(Level.DEBUG, "****Business ******  Debut Main ");
+        LogsProjet.geLogsInstance(App.class).maTrace(Level.DEBUG, "Appel Controlleur dbUserEntity");
         (new UserMgmt()).getListUsers();
+        LogsProjet.geLogsInstance(App.class).maTrace(Level.DEBUG, "****Business ****** Fin Main ");
     }
 }
--- business/src/main/java/fr/ocr/prj06/business/businessCtrl/UserMgmt.java
+++ business/src/main/java/fr/ocr/prj06/business/businessCtrl/UserMgmt.java
@@ -2,6 +2,8 @@ package fr.ocr.prj06.business.businessCtrl;
 
 import fr.ocr.prj06.entity.controller.UserJpaController;
 import fr.ocr.prj06.entity.stub.dbUserEntity;
+import fr.ocr.prj06.utility.logs.LogsProjet;
+import org.apache.logging.log4j.Level;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +20,8 @@ public class UserMgmt {
         ArrayList<String> stringArrayList = new ArrayList<>();
         for (dbUserEntity user : dbUserEntities) {
             String s = "rang : " + user.getIduser() + " ,  Nom : " + user.getNom() + " eMail :" + user.getEmail();
-            System.out.println(s);
             stringArrayList.add(s);
+            LogsProjet.geLogsInstance(UserMgmt.class).maTrace(Level.DEBUG, "--> Liste User : " + s);
         }
         return stringArrayList;
     }
