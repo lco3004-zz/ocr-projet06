7d44159c1254da5d2b61a9e3bb48fb5950929ac8
cordier <laurent.cordier3004@gmail.com>
2019-09-07 18:10:50 +0200

    refactor web.xml

--- src/main/java/fr/ocr/view/servlets/acceuil/Svt_AcceuilSite.java
+++ src/main/java/fr/ocr/view/servlets/acceuil/Svt_AcceuilSite.java
@@ -3,6 +3,7 @@ package fr.ocr.view.servlets.acceuil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Svt_AcceuilSite", urlPatterns = {"/AcceuilSite"})
+@WebServlet(name = "Svt_AcceuilSite", urlPatterns = {"/", "/home"})
 public class Svt_AcceuilSite extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -21,10 +22,12 @@ public class Svt_AcceuilSite extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        RequestDispatcher requestDispatcher = this.getServletContext().getNamedDispatcher("Jsp_AcceuilSite");
+        requestDispatcher.forward(request, response);
     }
 }
